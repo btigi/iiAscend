c2ce40d70bc2d206c133ea94d92d469be095b2a8
btigi <btigi@users.noreply.github.com>
2025-12-30 21:08:26 +0000

    Initial commit

---
+++ src/HogProcessor.cs
@@ -0,0 +1,41 @@
+﻿namespace ii.Ascend;
+
+public class HogProcessor
+{
+    public List<(string filename, byte[] bytes)> Read(string filename)
+    {
+        using var fileStream = new FileStream(filename, FileMode.Open, FileAccess.Read);
+        using var binaryReader = new BinaryReader(fileStream);
+
+        var signature = binaryReader.ReadBytes(3);
+        if (signature[0] != 'D' || signature[1] != 'H' || signature[2] != 'F')
+        {
+            throw new InvalidDataException("Invalid HOG file signature. Expected 'DHF'.");
+        }
+
+        var result = new List<(string filename, byte[] bytes)>();
+        while (binaryReader.BaseStream.Position < binaryReader.BaseStream.Length)
+        {
+            // 8.3 format filename
+            var filenameBytes = binaryReader.ReadBytes(13);
+            if (filenameBytes.Length < 13)
+            {
+                break;
+            }
+
+            var size = binaryReader.ReadUInt32();
+
+            var fileData = binaryReader.ReadBytes((int)size);
+            if (fileData.Length < size)
+            {
+                throw new EndOfStreamException($"Unexpected end of stream while reading file data. Expected {size} bytes, got {fileData.Length}.");
+            }
+
+            var filenameString = System.Text.Encoding.ASCII.GetString(filenameBytes).Split('\0')[0];
+
+            result.Add((filenameString, fileData));
+        }
+
+        return result;
+    }
+}
---
+++ src/TxbProcessor.cs
@@ -0,0 +1,33 @@
+﻿namespace ii.Ascend;
+
+public class TxbProcessor
+{
+    public string Read(string filename)
+    {
+        var fileBytes = File.ReadAllBytes(filename);
+        var decryptedBytes = new List<byte>();
+
+        foreach (var currentByte in fileBytes)
+        {
+            if (currentByte == 0x0A)
+            {
+                decryptedBytes.Add(0x0D);
+                decryptedBytes.Add(0x0A);
+            }
+            else
+            {
+                var xored = (byte)(currentByte ^ 0xE9);
+                var decrypted = RotateLeft(xored, 2);
+                decryptedBytes.Add(decrypted);
+            }
+        }
+
+        return System.Text.Encoding.UTF8.GetString(decryptedBytes.ToArray());
+    }
+
+    private static byte RotateLeft(byte value, int count)
+    {
+        count = count % 8;
+        return (byte)((value << count) | (value >> (8 - count)));
+    }
+}
