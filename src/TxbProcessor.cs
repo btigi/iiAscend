9ca12087c3b34cb2012d1a017bf8b71951d4c36b
btigi <btigi@users.noreply.github.com>
2025-12-31 16:59:17 +0000

    Add TXB writing support

--- src/TxbProcessor.cs
+++ src/TxbProcessor.cs
@@ -25,9 +25,40 @@ public class TxbProcessor
         return System.Text.Encoding.UTF8.GetString(decryptedBytes.ToArray());
     }
 
+    public void Write(string filename, string content)
+    {
+        var decryptedBytes = System.Text.Encoding.UTF8.GetBytes(content);
+        var encryptedBytes = new List<byte>();
+
+        for (int i = 0; i < decryptedBytes.Length; i++)
+        {
+            var currentByte = decryptedBytes[i];
+            
+            if (currentByte == 0x0D && i + 1 < decryptedBytes.Length && decryptedBytes[i + 1] == 0x0A)
+            {
+                encryptedBytes.Add(0x0A);
+                i++;
+            }
+            else
+            {
+                var rotated = RotateRight(currentByte, 2);
+                var encrypted = (byte)(rotated ^ 0xE9);
+                encryptedBytes.Add(encrypted);
+            }
+        }
+
+        File.WriteAllBytes(filename, encryptedBytes.ToArray());
+    }
+
     private static byte RotateLeft(byte value, int count)
     {
         count = count % 8;
         return (byte)((value << count) | (value >> (8 - count)));
+    }
+
+    private static byte RotateRight(byte value, int count)
+    {
+        count = count % 8;
+        return (byte)((value >> count) | (value << (8 - count)));
     }
 }
